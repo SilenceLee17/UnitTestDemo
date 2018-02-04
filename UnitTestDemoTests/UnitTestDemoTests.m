354d894b8bc6a37e7099cb7e20d40c6269d5c99a
lixingdong <lixingdong@foundersc.com>
2018-02-04 18:14:50 +0800

    add comments

--- UnitTestDemoTests/UnitTestDemoTests.m
+++ UnitTestDemoTests/UnitTestDemoTests.m
@@ -16,24 +16,30 @@
 
 - (void)setUp {
     [super setUp];
+    //    初始化的代码，在测试方法调用之前调用
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
+    //    释放测试用例的资源代码，每个方法在每个测试用例执行后调用。测试失败不调用。
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
+    //    测试用例
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
 - (void)testPerformanceExample {
+    //    性能测试用例
     // This is an example of a performance test case.
     [self measureBlock:^{
+        //        需要测试性能的代码
         // Put the code you want to measure the time of here.
     }];
 }
 
 @end
+
