900435b9b23c7a42b3283a854016f61bfadd9acb
lixingdong <lixingdong@foundersc.com>
2018-02-04 18:13:24 +0800

    add demo

---
+++ UnitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitTestDemo
+//
+//  Created by 李兴东 on 2018/2/4.
+//  Copyright © 2018年 xingshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitTestDemo
+//
+//  Created by 李兴东 on 2018/2/4.
+//  Copyright © 2018年 xingshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
